3025d807846505f1e8bb3177094e4e610b200c79
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-06 17:04:41 +0300

    Added callApi function

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -21,18 +21,29 @@ class AssignmentController extends Controller
         }
     }
 
-    public function countSeconds(){
-        $dat1 = mktime(4,14,1732);
+    public function countSeconds()
+    {
+        $dat1 = mktime(4, 14, 1732);
         $date2 = microtime(true);
         $diff = $dat1 - $date2;
 
         echo $diff;
-
-        
     }
 
-    
-
-
-
+    public function callApi()
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://icanhazdadjoke.com/slack");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 4);
+        $json = curl_exec($curl);
+        if (!$json) {
+            echo curl_error($curl);
+        }
+        curl_close($curl);
+        return response()->json([
+            "status" => "Success",
+            "api:" => json_decode($json)
+        ], 200);
+    }
 }
